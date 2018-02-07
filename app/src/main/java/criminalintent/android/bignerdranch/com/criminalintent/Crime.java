a2db87fd7ad3a8f57ffaa2587ed5a71197908671
dongtongtong <dongtongtong@meituan.com>
2018-02-07 20:45:24 +0800

    crimeFragment布局完成

---
+++ app/src/main/java/criminalintent/android/bignerdranch/com/criminalintent/Crime.java
@@ -0,0 +1,50 @@
+package criminalintent.android.bignerdranch.com.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by dt on 2018/2/6.
+ */
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime() {
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public UUID getId() {
+
+        return mId;
+    }
+
+}
